8c644e4a73b36d05e1b1ca3caafdcef30e07319b
moeta <teqqmu@gmail.com>
2021-10-23 20:01:23 +0700

    feat: create app-block

---
+++ src/components/app-block.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="
+      border border-gray-300 dark:border-gray-700
+      rounded
+      p-4
+      flex flex-col
+    "
+  >
+    <slot />
+  </div>
+</template>
