3503c2602777a1a1b452cd4d9936624b22be7052
moeta <teqqmu@gmail.com>
2021-10-23 20:11:05 +0700

    feat: create app-recomendation

---
+++ src/components/app-recomendation.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <app-title>⭐ recomendation</app-title>
+    <app-block>
+      Beastars
+    </app-block>
+  </div>
+</template>
