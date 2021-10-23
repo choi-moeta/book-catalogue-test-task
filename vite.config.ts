16a16abc5fe5279e9814df3f6b6039fab5408c0b
moeta <teqqmu@gmail.com>
2021-10-23 14:58:31 +0700

    feat: add unplugin-icons, unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    Vue(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          componentPrefix: 'icon',
+        }),
+      ],
+
+      dts: 'src/components.d.ts',
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
 })
