27a0f2e7731b16e790953d0cd9b574e5436c30b5
moeta <teqqmu@gmail.com>
2021-10-23 20:09:26 +0700

    feat: create app-books, app-book

---
+++ src/components/app-book.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Book } from '../firebase'
+
+defineProps<{book: Book}>()
+</script>
+
+<template>
+  <app-block class="whitespace-pre-wrap break-words">
+    {{ JSON.stringify(book, null, 2) }}
+  </app-block>
+</template>
---
+++ src/components/app-books.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useBooks } from '../firebase'
+
+const books = useBooks()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <app-title>📖 books</app-title>
+    <div class="grid gap-4 grid-cols-2 items-stretch">
+      <template v-for="book of books" :key="book.id">
+        <app-book :book="book"></app-book>
+      </template>
+    </div>
+  </div>
+</template>
