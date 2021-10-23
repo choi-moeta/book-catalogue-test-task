6b91dece043e0d95deaa731567cc36df020513a8
moeta <teqqmu@gmail.com>
2021-10-23 19:48:43 +0700

    feat: change title size

--- src/components/app-title.vue
+++ src/components/app-title.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-4xl font-bold text-black dark:text-white">
+  <h1 class="text-3xl font-semibold text-black dark:text-white">
     <slot />
   </h1>
 </template>
