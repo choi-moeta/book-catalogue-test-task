7e92fa3b2dd14c68d125e6f89c6ac1edb00c7f0a
moeta <teqqmu@gmail.com>
2021-10-23 20:11:47 +0700

    chore: create method for fetching books

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -1,5 +1,26 @@
-import { getFirestore } from 'firebase/firestore/lite'
+import { getFirestore, collection, doc, getDocs, query, setDoc, CollectionReference } from 'firebase/firestore/lite'
+import { usePromise } from 'vue-promised'
 import { app } from './app'
+import { BookData } from './types'
 
 export const db = getFirestore(app)
 
+export async function fetchBooks() {
+  const q = query<BookData>(collection(db, 'books') as CollectionReference<BookData>)
+  const snapshot = await getDocs(q)
+  const books = snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }))
+  return books
+}
+
+export function useBooks() {
+  const { data: books } = usePromise(fetchBooks())
+  return books
+}
+
+export async function createBook(book: BookData) {
+  const booksRef = collection(db, 'books')
+  return setDoc(doc(booksRef), book)
+}
