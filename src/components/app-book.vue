3e886072e3b76ba425b0815921afcaca12013d35
moeta <teqqmu@gmail.com>
2021-10-24 18:53:48 +0700

    feat: update app-book to look good

--- src/components/app-book.vue
+++ src/components/app-book.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import { Book } from '../firebase'
 
-defineProps<{book: Book}>()
+const props = defineProps<{book: Book}>()
+
+// [a,b,c] -> 'a, b and c'
+const authorsStr = computed(() => props.book.authors.reduce((acc, cur, i, arr) => i + 1 < arr.length ? `${acc}, ${cur}` : `${acc} and ${cur}`))
 </script>
 
 <template>
-  <app-block class="whitespace-pre-wrap break-words">
-    {{ JSON.stringify(book, null, 2) }}
+  <app-block class="flex flex-col gap-1">
+    <div class="flex gap-2 items-start justify-between">
+      <div class="font-semibold text-regular">
+        {{ book.name }}
+      </div>
+      <template v-if="book.rating !== undefined">
+        <div class="border border-regular px-2 pt-0.5 pb-1 rounded text-xs flex-shrink-0">
+          {{ book.rating }} / 10
+        </div>
+      </template>
+    </div>
+    <div class="text-dim">
+      By {{ authorsStr }} in {{ book.year }}
+    </div>
+    <template v-if="book.ISBN !== undefined">
+      <div class="text-xs self-end text-dim mt-auto" title="ISBN">
+        {{ book.ISBN }}
+      </div>
+    </template>
   </app-block>
 </template>
