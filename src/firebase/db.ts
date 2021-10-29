7c28a8cc450978e8d4626b24602ac6eb5fccd3ea
moeta <teqqmu@gmail.com>
2021-10-29 23:04:38 +0700

    feat: create crud operations

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -1,26 +1,54 @@
-import { getFirestore, collection, doc, getDocs, query, setDoc, CollectionReference } from 'firebase/firestore/lite'
-import { usePromise } from 'vue-promised'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  setDoc,
+  deleteDoc,
+  CollectionReference,
+} from 'firebase/firestore/lite'
 import { app } from './app'
 import { BookData } from './types'
 
 export const db = getFirestore(app)
 
+export const booksRef = collection(db, 'books') as CollectionReference<BookData>
+
 export async function fetchBooks() {
-  const q = query<BookData>(collection(db, 'books') as CollectionReference<BookData>)
-  const snapshot = await getDocs(q)
-  const books = snapshot.docs.map(doc => ({
+  const snap = await getDocs(booksRef)
+  const books = snap.docs.map(doc => ({
     id: doc.id,
     ...doc.data(),
   }))
   return books
 }
 
-export function useBooks() {
-  const { data: books } = usePromise(fetchBooks())
-  return books
+export async function fetchBook(id: string) {
+  const snap = await getDoc(doc(booksRef, id))
+  if (snap.exists()) {
+    return {
+      id: snap.id,
+      ...snap.data(),
+    }
+  }
+  return null
 }
 
 export async function createBook(book: BookData) {
-  const booksRef = collection(db, 'books')
-  return setDoc(doc(booksRef), book)
+  const bookDoc = doc(booksRef)
+  await setDoc(doc(booksRef), book)
+  return bookDoc
+}
+
+export async function editBook(id: string, book: BookData) {
+  const bookDoc = doc(booksRef, id)
+  await setDoc(bookDoc, book)
+  return bookDoc
+}
+
+export async function deleteBook(id: string) {
+  const bookDoc = doc(booksRef, id)
+  await deleteDoc(bookDoc)
+  return bookDoc
 }
