db732984933d3101d3935ac4e57647a19f4805c8
moeta <teqqmu@gmail.com>
2021-10-29 23:19:19 +0700

    feat: update styles of index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,14 +3,32 @@
 
 <template>
   <div
-    class="mx-auto px-8 flex flex-row-reverse gap-8 justify-center items-start"
+    class="
+      mx-auto px-8 pb-8
+      max-w-screen-xl
+      grid gap-8
+      grid-cols-1 md:grid-cols-[1fr,minmax(240px,320px)]
+    "
   >
-    <aside class="self-stretch">
-      <div class="flex flex-col gap-8 mt-100px sticky top-8">
+    <aside class="self-stretch md:order-2">
+      <div class="flex flex-col gap-8 md:mt-100px sticky top-4">
         <app-recomendation />
         <app-profile />
       </div>
     </aside>
-    <app-books class="mt-100px max-w-screen-lg w-full" />
+    <main class="flex flex-col md:mt-80px">
+      <div class="flex justify-between sticky top-0 bg-white dark:bg-hex-111 pt-4 pb-2">
+        <app-title>📚 books</app-title>
+        <app-button
+          tag="router-link"
+          to="/book/new"
+          class="flex items-center gap-2"
+          type="secondary"
+        >
+          New book
+        </app-button>
+      </div>
+      <app-books class="w-full md:order-1" />
+    </main>
   </div>
 </template>
