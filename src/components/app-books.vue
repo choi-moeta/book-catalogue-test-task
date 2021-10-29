c20c73ab5c4ad2587e5b5523394eb5a0239320aa
moeta <teqqmu@gmail.com>
2021-10-29 23:18:19 +0700

    feat: move to pinia, add grouping

--- src/components/app-books.vue
+++ src/components/app-books.vue
@@ -1,15 +1,48 @@
 <script setup lang="ts">
-import { useBooks } from '../firebase'
+import { Book } from '../firebase'
+import { useBooksStore } from '../store/books'
 
-const books = useBooks()
+const books = useBooksStore()
+
+const groupedBooks = computed(() => {
+  if (!books.isEmpty) {
+    const groupedObj = books.all!
+      .reduce<Record<number, Book[]>>(
+        (acc, cur) => acc[cur.year]
+          ? { ...acc, [cur.year]: [...acc[cur.year], cur] }
+          : { ...acc, [cur.year]: [cur] },
+        {},
+      )
+
+    return Object.entries(groupedObj)
+      .map(([year, books]) => ({ year: Number(year), books }))
+      .sort((a, b) => b.year - a.year)
+      .map(({ year, books }) => ({ year, books: books.sort((a, b) => a.name > b.name ? 1 : -1) }))
+  }
+  return []
+})
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <app-title>📖 books</app-title>
-    <div class="grid gap-4 grid-cols-2 items-stretch">
-      <template v-for="book of books" :key="book.id">
-        <app-book :book="book"></app-book>
+    <div class="flex flex-col gap-4">
+      <template v-for="(group, groupIdx) of groupedBooks" :key="group.year">
+        <details :open="groupIdx == 0" class="flex flex-col">
+          <summary class="text-bright mx-4 tracking-wider">
+            {{ group.year }}
+          </summary>
+          <div
+            class="
+              mt-2
+              grid grid-cols-1 lg:grid-cols-2
+              gap-4 items-stretch
+            "
+          >
+            <template v-for="book of group.books" :key="book.id">
+              <app-book :book="book"></app-book>
+            </template>
+          </div>
+        </details>
       </template>
     </div>
   </div>
