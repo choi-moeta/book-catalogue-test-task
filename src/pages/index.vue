f5ea9ef8c7d4dadffb6580d83c6f44484171435c
moeta <teqqmu@gmail.com>
2021-11-01 15:05:36 +0700

    feat: change index page styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,20 +4,22 @@
 <template>
   <div
     class="
-      mx-auto px-8 pb-8
+      mx-auto
+      px-4 pb-8 md:(px-8)
+      md:mt-100px
       max-w-screen-xl
-      grid gap-8
-      grid-cols-1 md:grid-cols-[1fr,minmax(240px,320px)]
+      grid gap-4 grid-cols-1
+      md:(gap-8 grid-cols-[1fr,minmax(240px,320px)])
     "
   >
     <aside class="self-stretch md:order-2">
-      <div class="flex flex-col gap-4 md:mt-100px sticky top-4">
+      <div class="flex flex-col gap-4 sticky top-0 pt-4">
         <app-title>⭐ Recommendation</app-title>
         <app-recommendation />
       </div>
     </aside>
-    <main class="flex flex-col md:mt-80px">
-      <div class="flex justify-between sticky top-0 bg-white dark:bg-hex-111 pt-4 pb-2">
+    <main class="flex flex-col">
+      <div class="flex justify-between sticky top-0 bg-white dark:bg-hex-111 pt-4 pb-4">
         <app-title>📚 books</app-title>
         <app-button
           tag="router-link"
